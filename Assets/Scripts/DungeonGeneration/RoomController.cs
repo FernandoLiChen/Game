91478596848d6ea4ecbbe2ba275d9cd4276d9055
Fernando Chen <fernando.lichen@outlook.com>
2024-04-04 21:32:56 -0600

    Added Room Generator

--- Assets/Scripts/DungeonGeneration/RoomController.cs
+++ Assets/Scripts/DungeonGeneration/RoomController.cs
@@ -12,15 +12,100 @@ public class RoomInfo
 
 public class RoomController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public static RoomController instance;
+
+    string currentWorldName = "Dungeon";
+
+    RoomInfo currentLoadRoomData;
+
+    Queue<RoomInfo> loadRoomQueue = new Queue<RoomInfo>();
+
+    public List<Room> loadedRooms = new List<Room>();
+
+    bool isLoadingRoom = false;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
     void Start()
     {
+        LoadRoom("Start", 0, 0);
+        LoadRoom("Empty", 1, 0);
+        LoadRoom("Empty", -1, 0);
+        LoadRoom("Empty", 0, 1);
+        LoadRoom("Empty", 0, -1);
+    }
+
+    void Update()
+    {
+        UpdateRoomQueue();
+    }
+
+    void UpdateRoomQueue()
+    {
+        if (isLoadingRoom)
+        {
+            return;
+        }
+
+        if (loadRoomQueue.Count == 0)
+        {
+            return;
+        }
+
+        currentLoadRoomData = loadRoomQueue.Dequeue();
+        isLoadingRoom = true;
+
+        StartCoroutine(LoadRoomRoutine(currentLoadRoomData));
+    }
+
+    public bool DoesRoomExist(int x, int y)
+    {
+        return loadedRooms.Find(item => item.X == x && item.Y == y) != null;
+    }
+
+    public void LoadRoom(string name, int x, int y)
+    {
+        if (DoesRoomExist(x, y))
+        {
+            return;
+        }
+        RoomInfo newRoomData = new RoomInfo();
+        newRoomData.name = name;
+        newRoomData.x = x;
+        newRoomData.y = y;
+
+        loadRoomQueue.Enqueue(newRoomData);
 
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadRoomRoutine(RoomInfo info) //para que no se laguen las escenas
     {
+        string roomName = currentWorldName + info.name;
+        AsyncOperation loadRoom = SceneManager.LoadSceneAsync(roomName, LoadSceneMode.Additive);
+        while (loadRoom.isDone == false)
+        {
+            yield return null;
+        }
+    }
 
+    public void RegisterRoom(Room room)
+    {
+        room.transform.position = new Vector3(
+        currentLoadRoomData.x * room.Width,
+        currentLoadRoomData.y * room.Height,
+        0);
+
+        room.X = currentLoadRoomData.x;
+        room.Y = currentLoadRoomData.y;
+        room.name = currentWorldName + "-" + currentLoadRoomData.name + " " + room.X + ", " + room.Y;
+        room.transform.parent = transform;
+
+        isLoadingRoom = false;
+
+        loadedRooms.Add(room);
     }
 }
